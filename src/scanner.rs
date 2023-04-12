260d26caace2e5dbdc291a89a55258779024ee6e
SourMilk <116470756+SourM1lk@users.noreply.github.com>
2023-04-12 22:05:15 +0000

    Fix

--- src/scanner.rs
+++ src/scanner.rs
@@ -1,12 +1,14 @@
 use std::net::{IpAddr, Ipv4Addr, SocketAddr};
 use tokio::{task, net::TcpStream};
+use tokio::time::{timeout, Duration};
 use crate::config::ScannerConfig;
 use crate::connection::connect_to_proxy;
 use crate::socks::SocksVersion;
-use tokio::time::{timeout, Duration};
+use crate::report::report_proxy;
 
 
-pub async fn scan_proxies(config: ScannerConfig) {
+
+pub async fn scan_proxies(config: ScannerConfig) -> Vec<SocketAddr>  {
     let (start_ip, end_ip) = config.ip_range;
     let socks_version = match config.socks_version {
         4 => SocksVersion::V4,
@@ -14,37 +16,41 @@ pub async fn scan_proxies(config: ScannerConfig) {
         _ => unreachable!(),
     };
 
+    let mut valid_proxies = Vec::new();
     let mut tasks = Vec::new();
 
     for ip in ip_range(start_ip, end_ip) {
         for port in 1..=65535 {
             let proxy_addr = SocketAddr::new(ip, port);
-            let target_addr = SocketAddr::new(IpAddr::V4(Ipv4Addr::new(8, 8, 8, 8)), 53); // For example, we can use Google DNS as the target
+            let target_addr = SocketAddr::new(IpAddr::V4(Ipv4Addr::new(8, 8, 8, 8)), 53);
 
             let socks_version = socks_version.clone();
             let task = task::spawn(async move {
-                // 5 second connect timeout
-                // TODO: Make a flag to set timeout duration
-                match timeout(Duration::from_secs(5), TcpStream::connect(proxy_addr)).await {
+                match timeout(Duration::from_secs(4), TcpStream::connect(proxy_addr)).await {
                     Ok(_) => {
-                        if let Ok(granted) = connect_to_proxy(proxy_addr, target_addr, socks_version).await {
+                        if let Ok(granted) = connect_to_proxy(proxy_addr, target_addr, socks_version.clone()).await {
                             if granted {
-                                println!("Proxy found: {:?}", proxy_addr);
+                                report_proxy(proxy_addr, &socks_version);
+                                return Some(proxy_addr);
                             }
                         }
                     }
                     Err(_) => {}
                 }
+                None
             });
-
             tasks.push(task);
         }
     }
 
-    // Wait for all tasks to complete
+    // Wait for all tasks to complete and collect valid proxies
     for task in tasks {
-        task.await.unwrap();
+        if let Some(proxy) = task.await.unwrap() {
+            valid_proxies.push(proxy);
+        }
     }
+
+    valid_proxies
 }
 
 fn ip_range(start_ip: IpAddr, end_ip: IpAddr) -> impl Iterator<Item = IpAddr> {
